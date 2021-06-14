b261832abd404a54615a58f79ec1b505edd36f41
ABIGAIL RODRÍGUEZ MOTA <ar2019-0095@uce.edu.do>
2021-06-14 13:31:54 -0400

    Filtro por pais y avances en filtro por categories

--- src/Components/NavbarComponent.jsx
+++ src/Components/NavbarComponent.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import DropdownCountry from "./DropdownCountry";
-import Select from "react-select";
+// import { countries } from "./DropdownCountry";
+// import Select from "react-select";
 
 export default class NavbarComponent extends Component {
   constructor(props) {
@@ -9,21 +9,22 @@ export default class NavbarComponent extends Component {
 
   handleGetCategorySubmit = (event) => {
     event.preventDefault();
-    // console.log("se hizo click");
-    this.props.onSearch(event.target.elements["catEntert"].value);
+    let idcategory = document.getElementById("catentert");
+    let cat = idcategory.value;
+    this.props.category.onSearch(cat);
+    console.log(cat);
   };
 
-  handleGetCountry = (e) => {
-    e.preventDefault();
-    // this.props.onSearch(e.target.elements["selectC"].value);
+  handleGetCountry = (event) => {
+    event.preventDefault();
+    let idcountry = document.getElementById("country");
+    let pro = idcountry.options[idcountry.selectedIndex].value;
+    this.props.onSearch(pro);
   };
 
   render() {
     return (
-      <nav
-        className="navbar navbar-expand-lg navbar-dark bg-dark"
-        onClick={this.handleGetCategorySubmit()}
-      >
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           {/* <a className="navbar-brand" href="#">
             NEWS APP
@@ -52,7 +53,7 @@ export default class NavbarComponent extends Component {
                 </a> */}
                 <button
                   type="button"
-                  name="catentert"
+                  id="catentert"
                   value="entertaiment"
                   className="btn btn-primary d-block"
                 >
@@ -92,19 +93,77 @@ export default class NavbarComponent extends Component {
                 </a>
               </li>
 
-              <li className="nav-item dropdown" style={{ width: "150px" }}>
+              <li
+                className="nav-item "
+                style={{ width: "150px" }}
+                onClick={this.handleGetCategorySubmit}
+              >
                 <select
-                  name="selectC"
-                  // defaultValue={countries[0]}
+                  id="country"
                   className="form-select"
                   aria-label="Default select example"
-                  // onChange={this.handleGetCountry}
-                  options={<DropdownCountry />}
-                ></select>
+                  onChange={this.handleGetCountry}
+                  //options={countries}
+                >
+                  <option> Country </option>
+                  <option value="ar"> Argentina </option>
+                  <option value="at"> Austria </option>
+                  <option value="au">Australia</option>
+                  <option value="be">Belgium</option>
+                  <option value="br">Brazil</option>
+                  <option value="bg">Bulgaria</option>
+                  <option value="ca">Canada</option>
+                  <option value="cn">China</option>
+                  <option value="co">Colombia</option>
+                  <option value="cu">Cuba</option>
+                  <option value="cz">Czech Republic</option>
+                  <option value="eg">Egypt</option>
+                  <option value="fr">France</option>
+                  <option value="de">Germany</option>
+                  <option value="gr">Greece</option>
+                  <option value="hk">Hong Kong</option>
+                  <option value="hu">Hungary</option>
+                  <option value="in">India</option>
+                  <option value="id">Indonesia</option>
+                  <option value="ie">Ireland</option>
+                  <option value="il">Israel</option>
+                  <option value="it">Italy</option>
+                  <option value="jp">Japan</option>
+                  <option value="lv">Latvia</option>
+                  <option value="lt">Lithuania</option>
+                  <option value="my">Malaysia</option>
+                  <option value="mx">Mexico</option>
+                  <option value="ma">Morocco</option>
+                  <option value="nl">Netherlands</option>
+                  <option value="nz">New Zealand</option>
+                  <option value="ng">Nigeria</option>
+                  <option value="no">Norway</option>
+                  <option value="ph">Philippines</option>
+                  <option value="pl">Poland</option>
+                  <option value="pt">Portugal</option>
+                  <option value="ro">Romania</option>
+                  <option value="ru">Russian Federation</option>
+                  <option value="sa">Saudi Arabia</option>
+                  <option value="rs">Serbia</option>
+                  <option value="sg">Singapore</option>
+                  <option value="sk">Slovakia</option>
+                  <option value="si">Slovenia</option>
+                  <option value="za">South Africa</option>
+                  <option value="kr">Korea</option>
+                  <option value="se">Sweden</option>
+                  <option value="ch">Switzerland</option>
+                  <option value="tw">Taiwan</option>
+                  <option value="th">Thailand</option>
+                  <option value="tr">Turkey</option>
+                  <option value="ua">Ukraine</option>
+                  <option value="ae">United Arab Emirates</option>
+                  <option value="gb">
+                    United Kingdom of Great Britain and Northern Ireland
+                  </option>
+                  <option value="us">United States of America</option>
+                  <option value="ve">Venezuela</option>
+                </select>
               </li>
-              {/* <li class="nav-item">
-                                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                            </li> */}
             </ul>
           </div>
         </div>
